8ceb7b89b67e9dd6c22876296195c8cd22e55a04
John Schmidley <tlg8n@outlook.com>
2019-09-19 03:53:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  new_array = []
+    names.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  new_array
+end
+
+def assign_rooms(attendees)
+  counter = 1
+  new_array = []
+  attendees.each do |names|
+    new_array.push(badge_maker(attendees) "Hello, #{name}! You'll be assigned to room #{attendees}!"
+    counter += 1
+  end
+  new_array
+end
